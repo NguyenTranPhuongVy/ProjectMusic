017b37774ee544f44913e28768d9376a9cc43b56
Nguyễn Trần Phương Vy <nguyentranphuongvy99@gmail.com>
2021-02-03 17:57:59 +0700

    Show Angular

--- ProjectMusicSound/Controllers/JsonUsersController.cs
+++ ProjectMusicSound/Controllers/JsonUsersController.cs
@@ -83,14 +83,46 @@ namespace ProjectMusicSound.Controllers
                 music_time = n.music_time,
                 music_view = n.music_view,
                 user_id = n.user_id,
-                namesinger = n.User.user_name
+                listarrsing = Singers(n.music_id)
             }).ToList();
             return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public string[] Singers(int? id)
+        {
+            List<string> myList = new List<string>();
+            List<Music_Singer> music_Singers = db.Music_Singer.Where(n => n.music_id == id).ToList();
+            foreach (var item in music_Singers)
+            {
+                myList.Add(item.Singer.singer_name);
+            }
+            string[] arr = myList.ToArray();
+            return arr;
         }
 
         public JsonResult NewAlbum()
         {
             List<Album> albums = db.Albums.Where(n => n.album_active == true && n.album_bin == false && n.User.role_id == 2).OrderByDescending(n => n.album_datecreate).Take(12).ToList();
+            List<AlbumJson> list = albums.Select(n => new AlbumJson
+            {
+                album_active = n.album_active,
+                album_bin = n.album_bin,
+                album_datecreate = n.album_datecreate.ToString(),
+                album_dateedit = n.album_dateedit.ToString(),
+                album_id = n.album_id,
+                album_img = n.album_img,
+                album_love = n.album_love,
+                album_name = n.album_name,
+                album_note = n.album_note,
+                album_view = n.album_view,
+                user_id = n.user_id
+            }).ToList();
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ViewAlbum()
+        {
+            List<Album> albums = db.Albums.Where(n => n.album_active == true && n.album_bin == false && n.User.role_id == 2).OrderByDescending(n => n.album_view).Take(12).ToList();
             List<AlbumJson> list = albums.Select(n => new AlbumJson
             {
                 album_active = n.album_active,
---
+++ ProjectMusicSound/Scripts/Json/User/Show.js
@@ -0,0 +1,35 @@
+﻿var app = angular.module('myApp', []);
+app.controller('myNewHit', function ($scope, $http) {
+    $http({
+        method: "GET",
+        url: "/JsonUsers/MusicShow"
+    }).then(function mySuccess(response) {
+        $scope.list = response.data;
+    }, function myError(response) {
+        $scope.list = response.statusText;
+    });
+});
+//Album mới
+app.controller('myAlbum', function ($scope, $http) {
+    $http({
+        method: "GET",
+        url: "/JsonUsers/NewAlbum"
+    }).then(function mySuccess(response) {
+        $scope.list = response.data;
+    }, function myError(response) {
+        $scope.list = response.statusText;
+
+    });
+});
+//Album theo view
+app.controller('myView', function ($scope, $http) {
+    $http({
+        method: "GET",
+        url: "/JsonUsers/ViewAlbum"
+    }).then(function mySuccess(response) {
+        $scope.list = response.data;
+    }, function myError(response) {
+        $scope.list = response.statusText;
+
+    });
+});
